60c0ff204dfb53e03cac106090f96e7b1d7bea05
mattstub <matt@mattstub.com>
2018-01-18 20:04:06 -0600

    completing day 13 -- slide in on scroll

---
+++ javascript30/d13/app.js
@@ -0,0 +1,34 @@
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function debounce(func, wait = 20, immediate = true) {
+  let timeout;
+  return function() {
+    let context = this; 
+    let args = arguments;
+    let later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    let callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function checkSlide(e) {
+  sliderImages.forEach(sliderImage => {
+    // Halfway through Image
+    const slideInAt = (window.scrollY + window.innerHeight) - sliderImage.height / 2;
+    // Bottom of Image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if(isHalfShown && isNotScrolledPast)
+      sliderImage.classList.add('active');
+    else
+      sliderImage.classList.remove('active');
+  });
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
---
+++ javascript30/d13/readme.md
@@ -0,0 +1,4 @@
+# Journal
+### 18 January 2018
+
+This is such a great lesson for learning more about window events and how to handle them. It may not be the most necessary thing to learn when to scroll an image in, however its great for learning how things happen. Also spending some time learning how the debounce function works properly, if not importing it from the *lodash* library is great!
