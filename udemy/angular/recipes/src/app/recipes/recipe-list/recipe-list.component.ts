313bf85010100c85e855d9fbf33caa86c945bb78
mattstub <matt@mattstub.com>
2018-01-22 06:05:51 -0600

    wrapping up recipe list component

--- udemy/angular/recipes/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy/angular/recipes/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Banana Bread', 'Best banana bread', 'https://23991-presscdn-pagely.netdna-ssl.com/wp-content/uploads/2015/12/banana-bread-recipe-fp.jpg'),
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Banana Bread', 'Best banana bread', 'https://23991-presscdn-pagely.netdna-ssl.com/wp-content/uploads/2015/12/banana-bread-recipe-fp.jpg')
+  ];
 
   constructor() { }
 
