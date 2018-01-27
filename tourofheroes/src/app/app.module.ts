cab7768e65e8708e1c9885fb82dd09edd4fcf27d
mattstub <matt@mattstub.com>
2018-01-27 09:31:40 -0600

    initial commit

---
+++ tourofheroes/src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
+
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroService } from './hero.service';
+import { MessagesComponent } from './messages/messages.component';
+import { MessageService } from './message.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+@NgModule({
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ) 
+  ],
+  declarations: [ 
+    AppComponent, 
+    HeroesComponent, 
+    HeroDetailComponent, 
+    MessagesComponent, DashboardComponent, HeroSearchComponent ],
+  bootstrap: [ 
+    AppComponent 
+  ],
+  providers: [ 
+    HeroService, 
+    MessageService, InMemoryDataService 
+  ]
+})
+export class AppModule { }
