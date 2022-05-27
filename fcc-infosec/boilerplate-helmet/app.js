a3a2dc131c7e625022face795c5718a0af5123bb
Matt Stubenhofer <matt.stubenhofer@gmail.com>
2022-05-27 09:49:35 -0500

    potentional DDOS alert from boilerplate

--- fcc-infosec/boilerplate-helmet/app.js
+++ fcc-infosec/boilerplate-helmet/app.js
@@ -21,6 +21,17 @@ const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
 app.use('/_api', api);
+
+// set up rate limiter: maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter);
+
 app.get("/", function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
