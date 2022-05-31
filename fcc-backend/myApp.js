473e79efdfa33eccf6b68fe28d9cb8f9298ef429
Matt Stubenhofer <matt.stubenhofer@gmail.com>
2022-05-31 08:41:34 -0500

    rate limiting middleware

--- fcc-backend/myApp.js
+++ fcc-backend/myApp.js
@@ -2,6 +2,16 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
 
+// set up rate limiter: maximum of five requests per minute
+let RateLimit = require('express-rate-limit');
+let limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// apply rate limiter to all requests
+app.use(limiter)
+
 console.log("Hello World");
 
 absolutePath = __dirname + '/views/index.html';
