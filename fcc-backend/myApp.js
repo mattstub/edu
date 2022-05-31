74a1f3c5c2d557e7d5c87be19c9eaa46ab323dc7
Matt Stubenhofer <matt.stubenhofer@gmail.com>
2022-05-31 08:36:03 -0500

    express boilerplate challenges

---
+++ fcc-backend/myApp.js
@@ -0,0 +1,57 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+
+console.log("Hello World");
+
+absolutePath = __dirname + '/views/index.html';
+assetsPath = __dirname + "/public";
+
+app.use("/public", express.static(assetsPath));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const timeMiddleware = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+app.get("/", (req, res) => {
+//  res.send("Hello Express");
+  res.sendFile(absolutePath);
+});
+
+app.get("/json", (req, res, next) => {
+  if(process.env.MESSAGE_STYLE == "uppercase")
+    res.json({ "message" : "HELLO JSON" });
+  else
+    res.json({ "message" : "Hello json" });
+  console.log(req.method + " " + req.path + " - " + req.ip);
+  next();
+});
+
+app.get('/now', timeMiddleware, (req, res) => {
+  res.send({
+    time: req.time
+  });
+});
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({ echo : word });
+});
+
+app.get("/name", (req, res) => {
+  let firstName = req.query.first;
+  let lastName = req.query.last;
+  res.json({ name : `${firstName} ${lastName}` });
+});
+
+app.post('/name', (req, res) => {
+  let firstName = req.body.first;
+  let lastName = req.body.last;
+  res.json({ name : `${firstName} ${lastName}` });
+});
+
+const mySecret = process.env['MESSAGE_STYLE']
+
+ module.exports = app;
